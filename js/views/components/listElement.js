e0123e5ba212ad351089b793ac60392558190f6e
Swapnil Raj <raj.swapnil99@gmail.com>
2017-08-04 22:08:53 +0530

    Add margin of 10px

--- js/views/components/listElement.js
+++ js/views/components/listElement.js
@@ -11,7 +11,7 @@ const listElement = ({ message }) => {
     const listElementView = new VElement('div', LIST_ELEMENT_CLASS);
     listElementView.element.innerHTML = message;
 
-    const miniContainer = new VElement('div', LIST_ELEMENT_CONTAINER_CLASS, {}, {}, [ listElementView.element ]);
+    const miniContainer = new VElement('div', LIST_ELEMENT_CONTAINER_CLASS, {margin: "10px"}, {}, [ listElementView.element ]);
 
     return miniContainer.element;
 };
